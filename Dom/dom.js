c22cc1a576f40841ea642a7be3c47156bc9c7116
Gopal pandey <ranjeetraj7544827919@gmail.com>
2023-09-26 19:34:17 +0530

    all changed

---
+++ Dom/dom.js
@@ -0,0 +1,17 @@
+//  The getElementById Method--------------------------------------
+
+// let data = document.getElementById('demo')
+// console.log(data.innerText);
+// data.innerText = 'Gopal pandey'
+
+//  The getElementsByTagName Method----------------------------------------
+
+// let data = document.getElementsByTagName('h1')
+// console.log(data[0].innerText);
+// data[0].innerText = 'Gopal'
+
+//  The getElementsByClassName Method----------------------------------------
+
+// let data = document.getElementsByClassName('demo')
+// console.log(data[0].innerText);
+// data[0].innerText = 'Gopal'
