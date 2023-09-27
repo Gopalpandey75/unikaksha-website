fe116749542bd12c79ab38bd540353c2a455fb19
Gopal pandey <ranjeetraj7544827919@gmail.com>
2023-09-28 00:12:13 +0530

    update

--- Dom/dom.js
+++ Dom/dom.js
@@ -15,3 +15,51 @@
 // let data = document.getElementsByClassName('demo')
 // console.log(data[0].innerText);
 // data[0].innerText = 'Gopal'
+
+//  The querySelector Method----------------------------------------
+
+
+// for class
+
+// let data = document.querySelector('.demo')
+// console.log(data.innerText);
+// data.innerText = 'Gopal'
+
+// for id
+
+// let data = document.querySelector('#demo')
+// console.log(data.innerText);
+// data.innerText = 'Gopal'
+
+//  The querySelectorAll Method----------------------------------------
+
+
+// for class
+
+// let data = document.querySelectorAll('.demo')
+// console.log(data);
+// for (let x of data) {
+//     console.log(x);
+//     x.innerHTML = 'lal kila'
+// }
+
+
+// The Attribute Method-------------------------------------------------------
+
+// 
+
+// For image change 
+
+// let data = document.querySelector("img").src = 'https://images.pexels.com/photos/3881106/pexels-photo-3881106.jpeg?auto=compress&cs=tinysrgb&w=600'
+
+// For style change
+
+// let heading= document.querySelector('h1').style='font-size:20px;background-color:red;'
+
+// click button to change pic
+
+// function myfunc(){
+//     document.querySelector('h1').innerHTML='new pic'
+//     document.querySelector("img").style="width:400px;"
+//     document.querySelector("img").src = 'https://images.pexels.com/photos/3881106/pexels-photo-3881106.jpeg?auto=compress&cs=tinysrgb&w=600'
+// }
