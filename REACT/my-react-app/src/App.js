3cd703f8af31c945bc00f5534db2fc14ac65ff1c
Gopal pandey <ranjeetraj7544827919@gmail.com>
2023-10-16 22:41:41 +0530

    react

---
+++ REACT/my-react-app/src/App.js
@@ -0,0 +1,76 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import React from 'react';
+
+// function App() {
+// return<h1>Hello, React!</h1>;
+
+
+// }
+
+// export default App;
+// ...............................................................
+// import React from 'react';
+// function App(){
+//   return(
+//     <div>
+//       <h1>Hello, React!</h1>
+//       <p>This is a complex JSX element.</p>
+//     </div>
+//   );
+// }
+// export default App;
+// ...............................................................
+
+import React from "react";
+function App(){
+  return(
+    <>
+    <h1>
+    Extramammary Paget DiseaseEnfermedad de Paget extramamaria
+    </h1>
+    <h2>Background and objective</h2>
+    <p>Extramammary Paget disease (EMPD) has seldom been studied in Mediterranean populations. We aimed to review the characteristics of our patients with EMPD, the presence of a neoplasm in continuity, and the long-term course of the disease.
+
+</p>
+<h2>Patients and methods</h2>
+    <p>Retrospective observational study of 27 patients diagnosed with EMPD between 1990 and 2015. All clinical and pathology findings related to clinical course and outcomes were retrieved for analysis.
+
+</p>
+<h2>Results</h2>
+    <p>Twenty patients were women and 7 were men. Ages ranged from 42 to 88 years (median, 76 years). Lesions were in the following locations: vulva (16 cases), pubis–groin (5), perianal region (4), and axilla (2). Time from onset to diagnosis ranged from 1 to 60 months (median, 12 months) and maximum lesion diameter from 20 to 140 mm (median, 55 mm). In 3 cases (11.1%) EMPD was a secondary condition. None of the lesions developed on a previous cutaneous adnexal adenocarcinoma. Ten of the 24 primary EMPDs (41.7%) invaded the dermis. Eight of the 27 patients (29.6%) experienced local recurrence after the initial surgical treatment. Three patients (11.1%) died as a consequence of metastasis from the EMPD.
+
+</p>
+<h2>Conclusions</h2>
+    <p>The presence of an underlying cutaneous adnexal adenocarcinoma is uncommon, but it is not unusual to find an extracutaneous adenocarcinoma in continuity. Although EMPD is a slow-growing tumor, dermal invasion is frequent and metastasis is not uncommon. Local recurrence is common even after excision with wide margins and may be delated, so long term follow-up is essential.
+
+</p>
+    </>
+  )
+}
+export default App;
+
